aee37ddd06bf44a4cebdc7461eb2db061c1ac0a0
Brian Shimanuki <bshimanuki@mit.edu>
2017-12-13 01:09:54 -0500

    ycm for more languages

--- vim/.vim/ycm/ycm_install.sh
+++ vim/.vim/ycm/ycm_install.sh
@@ -1,1 +1,1 @@
-$(dirname $0)/YouCompleteMe/install.py --clang-completer --gocode-completer --tern-completer --js-completer --rust-completer
+$(dirname $0)/YouCompleteMe/install.py --clang-completer --gocode-completer --tern-completer --js-completer
